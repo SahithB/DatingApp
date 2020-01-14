fd7d07b2fe6b4e5eeb00bc6aeac50df9cebc705f
Sahith <sahith_b@outlook.com>
2020-01-14 15:46:53 +0530

    Created Authcontroller with repository pattern and added user DTO to sln

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -30,6 +30,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,11 +42,11 @@ namespace DatingApp.API
             }
 
             //app.UseHttpsRedirection();
-            
+
 
             app.UseRouting();
 
-            app.UseCors( x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthorization();
 
